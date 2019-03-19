34feb8562d27d32542648b19fa0a288c4f4ef801
CARRYME0820 <744058944@qq.com>
2019-03-19 11:09:31 +0800

    springcloud begin

---
+++ eurekaconfig/src/main/java/com/edward/personal/eurekaconfig/EurekaconfigApplication.java
@@ -0,0 +1,17 @@
+package com.edward.personal.eurekaconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaconfigApplication.class, args);
+	}
+
+}
---
+++ eurekaconfig/src/test/java/com/edward/personal/eurekaconfig/EurekaconfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.edward.personal.eurekaconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaconfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
